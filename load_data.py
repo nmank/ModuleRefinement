ed036cde24e692a0b74ad43814b41043479093a9
nmank <nathan.mankovich@gmail.com>
2022-12-13 21:49:30 -0700

    fix load data gse bug

--- load_data.py
+++ load_data.py
@@ -45,12 +45,12 @@ def generate_gse73072_dataset(dPath, tBin, study, out_dir):
     subject_labels = pd.DataFrame(data = subjects, columns = ['subject'], index = sample_ids)
     subject_labels.index.name = 'SampleID'
 
-    labels = ccd.generate_labels('shedding', idx_list=idx)
+    labels = ccd.generate_labels(classificationAttr, idx_list=idx) == classificationAttr
     all_labels = pd.DataFrame(data = labels, columns = ['label'], index = sample_ids)
     all_labels.index.name = 'SampleID'
 
-    subject_labels.to_csv(f'{out_dir}gse73072_hrv_{tBin}_subjects.csv')
-    all_data.to_csv(f'{out_dir}gse73072_hrv_{tBin}.csv')
+    # subject_labels.to_csv(f'{out_dir}gse73072_hrv_{tBin}_subjects.csv')
+    # all_data.to_csv(f'{out_dir}gse73072_hrv_{tBin}.csv')
     all_labels.to_csv(f'{out_dir}gse73072_hrv_{tBin}_labels.csv')
 
 def generate_ebola_dataset(tissue, out_path):
@@ -97,8 +97,8 @@ if __name__ == '__main__':
     out_dir = '/data4/mankovic/ModuleRefinement/data/'
     generate_gse73072_dataset(dPath, tBin, study, out_dir)
 
-    generate_ebola_dataset('Liver', out_dir)
-    generate_ebola_dataset('Spleen', out_dir)
+    # generate_ebola_dataset('Liver', out_dir)
+    # generate_ebola_dataset('Spleen', out_dir)
 
-    generate_salmonella_dataset('Liver', out_dir)
-    generate_salmonella_dataset('Spleen', out_dir)
+    # generate_salmonella_dataset('Liver', out_dir)
+    # generate_salmonella_dataset('Spleen', out_dir)
