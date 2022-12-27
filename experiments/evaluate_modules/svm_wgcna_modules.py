83f6f62f8ddbdf7946ebf6058d1aa441c54430fe
Nathan Mankovich <nmank@master.slipstick.colostate.edu>
2022-12-27 05:40:01 -0700

    svm bug fixed

--- experiments/evaluate_modules/svm_wgcna_modules.py
+++ experiments/evaluate_modules/svm_wgcna_modules.py
@@ -11,6 +11,9 @@ from orthrus.core.dataset import DataSet as DS
 
 import os
 
+import sys
+sys.path.append('/home/nmank/ModuleRefinement/ModuleRefinement')
+
 import utils as utl
 
 import argparse
@@ -20,9 +23,9 @@ import argparse
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_dir", default = '/home/nmank/ModuleRefinement/data', type = str, help = "path to data directory")
-    parser.add_argument("--modules_dir", default = '/home/nmank/ModuleRefinement/experiments/modules', type = str, help = "path to modules directory")
-    parser.add_argument("--results_file", default = '/home/nmank/ModuleRefinement/experiments/results/svm_wgcna_score.csv', type = str, help = "path to results file")
+    parser.add_argument("--data_dir", default = './data', type = str, help = "path to data directory")
+    parser.add_argument("--modules_dir", default = 'experiments/modules', type = str, help = "path to modules directory")
+    parser.add_argument("--results_file", default = 'experiments/results/svm_wgcna_score.csv', type = str, help = "path to results file")
     args = parser.parse_args()
 
     data_dir = args.data_dir
@@ -54,48 +57,49 @@ if __name__ == '__main__':
         folder_path = f'{modules_dir}/{folds}'
 
         for dataset_name in os.listdir(folder_path):
-            print(f'doing modules {module_path}')
-            module_path = f'{folder_path}/{dataset_name}'
+            if ('ebola' not in dataset_name) and ('Spleen' not in dataset_name):
+                module_path = f'{folder_path}/{dataset_name}'
+                print(f'doing modules {module_path}')
 
-            if 'fold' in dataset_name:
-                fold = dataset_name[4]
-                dataset_name = dataset_name[6:-7]
-            else:
-                dataset_name = dataset_name[:-7]
-                fold = 'all'
+                if 'fold' in dataset_name:
+                    fold = dataset_name[4]
+                    dataset_name = dataset_name[6:-7]
+                else:
+                    dataset_name = dataset_name[:-7]
+                    fold = 'all'
             
-            if 'gse' in dataset_name:
-                organism = 'human'
-            else:
-                organism = 'mouse'
+                if 'gse' in dataset_name:
+                    organism = 'human'
+                else:
+                    organism = 'mouse'
 
-            data_name = utl.shorten_data_name(dataset_name)
-            ds = datasets[data_name]
+                data_name = utl.shorten_data_name(dataset_name)
+                ds = datasets[data_name]
 
-            the_modules, all_features = utl.load_modules(module_path)
+                the_modules, all_features = utl.load_modules(module_path)
 
             
-            module_number = 0
-            for module in the_modules.iterrows():
-                mod_sig = 0
-                module_genes = module[1].item()   
+                module_number = 0
+                for module in the_modules.iterrows():
+                    mod_sig = 0
+                    module_genes = module[1].item()   
 
-                module_size = len(list(module_genes))
+                    module_size = len(list(module_genes))
 
-                slice_dataset = ds.slice_dataset(feature_ids=np.array(module_genes))
+                    slice_dataset = ds.slice_dataset(feature_ids=np.array(module_genes))
 
-                try:
-                    bsr = utl.loso_test(slice_dataset, fold)
-                except ValueError:
-                    bsr = np.nan   
+                    try:
+                        bsr = utl.loso_test(slice_dataset, fold)
+                    except ValueError:
+                        bsr = np.nan   
                             
-                row = pd.DataFrame(columns = list(svm_results.columns),
+                    row = pd.DataFrame(columns = list(svm_results.columns),
                                     data = [[dataset_name, algorithm, np.nan,
                                                 np.nan, np.nan, fold,
                                                 module_number, bsr, module_size]])
-                svm_results = svm_results.append(row, ignore_index = True)
-                print(f'module {module_number} done')
-                module_number +=1
+                    svm_results = svm_results.append(row, ignore_index = True)
+                    print(f'module {module_number} done')
+                    module_number +=1
 
 
     svm_results.to_csv(results_file)
